0fcbc4ac5a8e5c2bfabb17428b50c877a1e0738a
shreeramjadhav564 <shreeramjadhav564@gmail.com>
2025-07-26 14:51:09 +0530

    "util updates"

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+import { app } from './app.js';
+
 import dotenv from 'dotenv';
 dotenv.config();
 
 import connectDB from './db/index.js';
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000, ()=>{
+        console.log(`Server running on port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed!!", err);  
+})
